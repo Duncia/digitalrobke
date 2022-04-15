f4851a93c5606378f753463319e5c9efbf833d2e
Robke <robertas.duncia@gmail.com>
2022-04-15 16:46:24 +0300

    Home page almost done

--- template-parts/call-to-contact.php
+++ template-parts/call-to-contact.php
@@ -1,7 +1,7 @@
 <section class="container call-to-contact">
     <div class="container__inner pt-md pb-md">
-        <h2 class="heading heading--2 clr-white pb-sm"><?php _e('Questions? Suggestions?', 'websitewizards'); ?></h2>
-        <p class="paragraph clr-white pb-sm"><?php _e('Always trying to reply to messages really fast!', 'websitewizards'); ?></p>
+        <h2 class="heading heading--2 clr-white pb-sm"><?php _e('Interested in cooperation?', 'websitewizards'); ?></h2>
+        <p class="paragraph clr-white pb-sm"><?php _e('Maybe have some questions? Please drop me a message!', 'websitewizards'); ?></p>
         <a class="btn btn--white" href="<?php _e('Some link', 'websitewizards'); ?>"><?php _e('Contact me', 'websitewizards'); ?></a>
     </div>
 </section>
